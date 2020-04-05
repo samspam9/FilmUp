89eee97f3d48df37b2237bdd800aaabc063ade07
Samir Ouafi <samir.ouafi@epitech.eu>
2020-04-05 21:40:18 +0200

    [ADD] movie list

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,31 +1,67 @@
-import * as WebBrowser from "expo-web-browser";
 import * as React from "react";
-import { Image, Platform, StyleSheet, Text, View } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { Image, StyleSheet, View, Button } from "react-native";
+import dataSource from "../sampleData.json";
+import { FlatGrid } from "react-native-super-grid";
 
 import { MonoText } from "../components/StyledText";
+import { TouchableHighlight } from "react-native";
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}
-      ></ScrollView>
-    </View>
-  );
+class HomeScreen extends React.Component {
+  goToMovie() {
+    console.log("going to movie page");
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatGrid
+          itemDimension={130}
+          items={dataSource.movies}
+          renderItem={({ item }) => (
+            <TouchableHighlight onPress={this.goToMovie}>
+              <View style={styles.movieContainer}>
+                <Image
+                  style={styles.imagePreview}
+                  source={{ uri: item.thumbnail }}
+                />
+                <View style={styles.textContainer}>
+                  <MonoText style={styles.orangeText}>{item.title}</MonoText>
+                </View>
+              </View>
+            </TouchableHighlight>
+          )}
+        />
+      </View>
+    );
+  }
 }
 
 HomeScreen.navigationOptions = {
   header: null,
 };
 
+export default HomeScreen;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#171717",
   },
   contentContainer: {
     paddingTop: 30,
+  },
+  imagePreview: {
+    width: 150,
+    height: 250,
+  },
+  orangeText: {
+    color: "orange",
+    textAlign: "center",
+  },
+  movieContainer: {
+    width: 150,
+  },
+  textContainer: {
+    backgroundColor: "#000000",
   },
 });
